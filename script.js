9de46860423fc59a1f54fb2d390e7f12ac6d9d95
Josh Webb <joshcwebb28@gmail.com>
2017-04-01 23:25:56 +0100

    Redo

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 $(function(){
 
+var w = window.innerWidth;
 
+  $('.home').click(function (event) {
+    event.preventDefault();
+    $(".homebox").scrollView();
+  });
+
+
+  $('.life').click(function (event) {
+    event.preventDefault();
+    $(".lifebox").scrollView();
+  });
+
+  $('.contact').click(function (event) {
+    event.preventDefault();
+    $(".contactbox").scrollView();
+  });
+
+  $('p').click(function (event) {
+    event.preventDefault();
+    $(".footer").scrollView();
+  });
+
+$.fn.scrollView = function () {
+  return this.each(function () {
+    $('html, body').animate({
+      scrollTop: $(this).offset().top-80
+    }, 1000);
+  });
+}
 
 });
